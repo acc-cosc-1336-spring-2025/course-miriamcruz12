b7f9c8c496ceb790b96fd177adea62e6d8c91326
Miriam Cruz <miriam.cruz@g.austincc.edu>
2025-02-08 05:48:43 +0000

    HW2

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,24 @@
 #
+def get_options_ratio(option, total): 
+
+    ratio = option/total
+    
+    return ratio
+
+def get_faculty_rating(ratio):
+
+    faculty_rating = ""
+
+    if(ratio >= .9 and ratio < 1):
+        faculty_rating = "Excellent"
+    elif(ratio > .8):
+        faculty_rating = "Very Good"
+    elif(ratio > .7):
+        faculty_rating = "Good"
+    elif(ratio > .6):
+        faculty_rating = "Needs Improvement"
+    elif(ratio > 0 and ratio <= .59):
+        faculty_rating = "Unacceptable"
+
+        return faculty_rating
+    
